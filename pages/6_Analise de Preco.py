0d4afbfe8e0522eb104e07825c5d22f9d216569d
Silas7005 <ribeirosilas05@gmail.com>
2023-11-12 11:20:43 -0300

    finish primeiras analises

---
+++ pages/6_Analise de Preco.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from utils.build import build_header
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+from utils.charts import boxplot,scatter,treemap,hist,bar,select_chart
+from utils.build import breakrows
+
+
+build_header(
+    title='Analise de preco',
+    hdr='# Analise de preço dos Veiculos',
+    p='''
+        <p> Nessa analise  buscamos entender onde a influencia do ano do veiculo e quais regioes temos a maior concetração de veiculos  e marcas </p>
+    '''
+)
+
+data = pd.read_parquet('data\price_cars10k.parquet')
+
+data_preco = data.groupby(['preco','marca', 'ano', 'modelo','estado','cidade']).size().reset_index(name='Total')
+data_preco.sort_values('Total', ascending=True, inplace=True)
+data_cars = data[['ano', 'preco', 'marca', 'modelo']]
+
+with st.expander("ViSUALIZAR OS DADOS DESTA SEÇÃO"):
+    _, c2, _ = st.columns((1,7,1))
+    c2.write(data_cars)
+
+breakrows()
+
+
+
+
+boxplot(
+    data,
+    x='preco',
+    title='BOXPLOT DOS PRECOS',
+    p='''Nesse boxplot  vemos a distribuicao dos precos dos veiculos , 
+    temos uma concentração maior de veiculos entre 13 mil e 26 mil reais.''')
+
+breakrows()
+
+scatter(
+    data= data,
+    x='preco',
+    title='Scatter do Preço pela Quilometragem',
+    y='quilometragem',
+    p='''vemos a relação entre o preço e a quilometragem rodada do veiculo,
+      os veiculos com maior quilometragem tendem a ter o valor menor, 
+      podemos considerar uma correlação negativa entre essas variaveis, 
+      mas temos que levar em conta outras variaveis pois a correlação nao implica causalidade,
+        o ano e modelo sao deterministico na precificação final'''
+)
+
+treemap(
+  data=data_preco, 
+  options=data.columns.to_list(),
+  default=data.columns.to_list()[:2],
+)
--- utils/build.py
+++ utils/build.py
@@ -8,3 +8,18 @@ def build_header(hdr: str, title: str, layout='wide',side='auto', p: str=''):
     )
     st.write(hdr)
     st.markdown(p,unsafe_allow_html=True)
+
+
+def breakrows():
+    st.write('''
+            <br>
+            <br>
+            <br>
+            <br>
+            ''',unsafe_allow_html=True)
+    
+#filtragem dos dados pelo top definido
+def top_categories(data, top: int, label: str):
+    top_make_data = data[label].value_counts().nlargest(top).index
+    filtered_data = data[data[label].isin(top_make_data)]
+    return filtered_data
