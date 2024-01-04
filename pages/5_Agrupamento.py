de5f26137e7829679001159d1580da1e52b61395
Silas7005 <ribeirosilas05@gmail.com>
2024-01-04 15:34:08 -0300

    modifier struct file

--- pages/5_Agrupamento.py
+++ pages/5_Agrupamento.py
@@ -20,31 +20,30 @@ build_header(
         <p> Nessa etapa buscamos encontrar o numero ideal de clusters para obtermos insights com base nesses grupos criados, os grupos sao formados por corelacoes entre as variaveis e separadas em grupos que divergem entre si</p>
     '''
 )
+def processing_data(data,target:str=''):
+    if target !='':
+        columns_drop = ['preco','preco_categoria']
+        x_data= data.drop(columns_drop, axis=1).values
+        y_data= data[target].values
+        tam = len(data.columns)-2
+    else:
+        columns_drop = ['preco']
+        x_data= data.drop(columns_drop, axis=1).values
+        y_data = ''
+        tam = len(data.columns)-1
+    le = LabelEncoder()
+    columns_categories = []
+    for x in range(tam):
+        if not((type(x_data[0, x]) == float) or (type(x_data[0, x]) == int)):
+            x_data[:, x] = le.fit_transform(x_data[:, x])
+            columns_categories.append(x)
+    scaler = StandardScaler()
+    x_data = scaler.fit_transform(x_data)
 
-target = ''
-if target !='':
-    columns_drop = ['preco','preco_categoria']
-    X_data= data.drop(columns_drop, axis=1).values
-    y_data= data[target].values
-else:
-    columns_drop = ['preco']
-    X_data= data.drop(columns_drop, axis=1).values
-    y_data = ''
+    return x_data, y_data, columns_categories
 
-le = LabelEncoder()
-columns_categories = []
-if target=='':
-    tam = len(data.columns)-1
-else:
-    tam = len(data.columns)-2
 
-for x in range(tam):
-    if not((type(X_data[0, x]) == float) or (type(X_data[0, x]) == int)):
-        X_data[:, x] = le.fit_transform(X_data[:, x])
-        columns_categories.append(x)
-
-scaler = StandardScaler()
-X_data = scaler.fit_transform(X_data)
+X_data, y_data, columns_categories = processing_data(data,target='')
 
 
 clr = st.sidebar.slider('Escolha a quantidade de Clusters', 0,10,1)
@@ -56,6 +55,7 @@ def chart_wcss(x_data):
     kmeans.fit(x_data)
     wcss.append(kmeans.inertia_)
   graph_wcss = px.line(x= range(1,11), y=wcss)
+  st.markdown('#### WCSS')
   st.plotly_chart(graph_wcss)
   return wcss
 
@@ -69,6 +69,7 @@ def kmeans(x_data,nclusters:int):
   pca = PCA(n_components=2)
   X_pca = pca.fit_transform(x_data)
   graph_clusters = px.scatter(x= X_pca[:,0], y= X_pca[:,1], color=labels)
+  st.markdown('#### Clusterizacao usando KMeans')
   st.plotly_chart(graph_clusters)
 
 kmeans(X_data, nclusters=clr)
